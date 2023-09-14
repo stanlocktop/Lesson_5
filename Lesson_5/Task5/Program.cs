178f2173ba5135b56983ad1be40608206916d6a7
stanlocktop <wlad-test@ukr.net>
2023-09-14 22:50:00 +0300

    finish task 5 and finish homeWork

--- Lesson_5/Task5/Program.cs
+++ Lesson_5/Task5/Program.cs
@@ -1,43 +1,50 @@
 ﻿using System;
-
+//5 *) Вказано п'ять довільних цілих чисел (елементи масиву). Визначити, чи є їхнє розташування в масиві впорядкованим (тобто за зростанням або за спаданням) чи невпорядкованим.
 class Program
 {
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11 };
-        Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
-        Console.WriteLine("Першоначальний вигляд масиву: ");
-        Console.WriteLine(String.Join(",", numbers));
-        Console.ResetColor(); // сбрасываем в стандартный
-        Console.ForegroundColor = ConsoleColor.Magenta; // устанавливаем цвет
-        Console.WriteLine("Введіть позицію для видалення елементу в масиві від 0 до {0}:", numbers.Length - 1);
-        int position = int.Parse(Console.ReadLine());
-        Console.ResetColor(); // сбрасываем в стандартный
-        if (position >= 0 && position < numbers.Length)
         {
-            for (int i = position; i < numbers.Length - 1; i++)
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            int[] numbers = { 3, 7, 5, 9, 4, 6, 1, -8 };
+            Console.WriteLine("Вигляд масиву: ");
+            Console.WriteLine(String.Join(",", numbers));
+            Console.ResetColor(); // сбрасываем в стандартный
+            bool isMoreArray = true;
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                numbers[i] = numbers[i + 1];
+                if (numbers[i] > numbers[i + 1])
+                {
+                    isMoreArray = false;
+                    break;
+                }
             }
-            Array.Resize(ref numbers, numbers.Length - 1);
+            bool isLessArray = true;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] < numbers[i + 1])
+                {
+                    isLessArray = false;
+                    break;
+                }
+            }
             Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
-            Console.WriteLine("Елемент на позиції {0} був видалений! ^_^ ", position);
-            Console.ResetColor(); // сбрасываем в стандартный
+            Console.WriteLine();
+            Console.WriteLine("Результат: ");
+            if (isMoreArray)
+            {
+                Console.WriteLine("Масив впорядкований за зростанням.");
+            }
+            else if (isLessArray)
+            {
+                Console.WriteLine("Масив впорядкований за спаданням.");
+            }
+            else
+            {
+                Console.WriteLine("Масив невпорядкований.");
+            }
+            Console.ReadKey();
         }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
-            Console.WriteLine("Немає такої позиції!");
-            Console.ResetColor(); // сбрасываем в стандартный
-        }
-        Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
-        Console.WriteLine("Оновлений масив: ");
-        Console.ResetColor(); // сбрасываем в стандартный
-        foreach (int number in numbers)
-        {
-            Console.Write(number + " ");
-        }
-        Console.ReadKey();
     }
 }
