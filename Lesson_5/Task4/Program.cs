bbc970e0e3ac086951d91eccfe7a38e54c6cb2b4
stanlocktop <wlad-test@ukr.net>
2023-09-14 22:08:26 +0300

    finish 4 task

--- Lesson_5/Task4/Program.cs
+++ Lesson_5/Task4/Program.cs
@@ -1,37 +1,43 @@
 ﻿using System;
-using System.Linq;
-//3) Написати програму створення масиву із двох масивів. У підсумковому масиві мають бути елементи першого та другого масиву
 
-public static class Extension
+class Program
 {
-    public static T[] Concatenate<T>(this T[] first, T[] second)
+    static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        if (first == null)
-        {
-            return second;
-        }
-        if (second == null)
-        {
-            return first;
-        }
-
-        return first.Concat(second).ToArray();
-    }
-}
-public class Example
-{
-    public static void Main()
-    {
-        int[] first = { 1, 2, 3, 4, 5, };
-        int[] second = { 6, 7, 8, 9, 10 };
-        int[] result = first.Concatenate(second);
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11 };
         Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
-        Console.WriteLine("Результат зливання першого та другого масивів: ");
-        Console.WriteLine(String.Join(",", result));
+        Console.WriteLine("Першоначальний вигляд масиву: ");
+        Console.WriteLine(String.Join(",", numbers));
         Console.ResetColor(); // сбрасываем в стандартный
+        Console.ForegroundColor = ConsoleColor.Magenta; // устанавливаем цвет
+        Console.WriteLine("Введіть позицію для видалення елементу в масиві від 0 до {0}:", numbers.Length - 1);
+        int position = int.Parse(Console.ReadLine());
+        Console.ResetColor(); // сбрасываем в стандартный
+        if (position >= 0 && position < numbers.Length)
+        {
+            for (int i = position; i < numbers.Length - 1; i++)
+            {
+                numbers[i] = numbers[i + 1];
+            }
+            Array.Resize(ref numbers, numbers.Length - 1);
+            Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
+            Console.WriteLine("Елемент на позиції {0} був видалений! ^_^ ", position);
+            Console.ResetColor(); // сбрасываем в стандартный
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+            Console.WriteLine("Немає такої позиції!");
+            Console.ResetColor(); // сбрасываем в стандартный
+        }
+        Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+        Console.WriteLine("Оновлений масив: ");
+        Console.ResetColor(); // сбрасываем в стандартный
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
         Console.ReadKey();
     }
 }
-
-
